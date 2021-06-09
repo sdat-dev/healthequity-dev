e397a390fe6f74af45f47fec95e31610d7a0bd75
sdat-dev <sdat@albany.edu>
2021-06-09 14:53:32 -0400

    Updated agenda-full link to git

--- js/agendafull.js
+++ js/agendafull.js
@@ -1,4 +1,4 @@
-let requestURL = "../data/agendafull.json";
+let requestURL = "https://sdat-dev.github.io/healthequity-dev/data/agendafull.json";
 let request = new XMLHttpRequest();
 //getting content Element to append grants information
 let maincontentContainer = document.getElementsByClassName('main-content')[0];
