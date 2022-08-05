7d6bd2f482504cc270da45e60eba252832613ad1
Bandla <rbandla@albany.edu>
2022-08-05 10:53:27 -0400

    label changes

--- js/topheader.js
+++ js/topheader.js
@@ -92,13 +92,13 @@ let addTopHeader = function () {
                             '</li>'+
                             '<li class="appear"><a href="https://ualbanysports.com/">Athletics</a></li>'+
                         '</ul>'+
-                        '<ul>'+
-                            '<div class="view-content" style="width: 110%;position: static;margin-left: -80px;padding-top: 0.6em;line-height: 1.2em;padding-bottom: 0.6em;background-color: #edba32;text-align: center">'+
-                                '<a href="/healthequity-dev/jobopportunity.html">'+
-                                    '<span style="color: black;font-weight: 600;font-size: 1.0em;" class="alert-title">Call for UAlbany Social and Health Equity Leader! <span style="color: black;font-weight: 600;font-size: 1.0em;">Learn more&gt;</span></span>'+
-                                '</a>'+
-                            '</div>'+
-                        '</ul>'+
+                        // '<ul>'+
+                            // '<div class="view-content" style="width: 110%;position: static;margin-left: -80px;padding-top: 0.6em;line-height: 1.2em;padding-bottom: 0.6em;background-color: #edba32;text-align: center">'+
+                            //     '<a href="/healthequity-dev/jobopportunity.html">'+
+                            //         '<span style="color: black;font-weight: 600;font-size: 1.0em;" class="alert-title">Call for UAlbany Social and Health Equity Leader! <span style="color: black;font-weight: 600;font-size: 1.0em;">Learn more&gt;</span></span>'+
+                            //     '</a>'+
+                            // '</div>'+
+                        // '</ul>'+
                     '</nav>'+
 
                 '</div>'+
